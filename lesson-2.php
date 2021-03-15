dbc7f6a79d9af384571d5c68c2992544d6518dd9
NONstop5 <non_2002@mail.ru>
2021-03-15 21:03:42 +0300

    add lesson-2.php to make second item of the task

--- lesson-2.php
+++ lesson-2.php
@@ -16,3 +16,58 @@ if (($a < 0 && $b >= 0) || ($a >= 0 && $b < 0)) {
     print('Сумма: ' . ($a + $b));
 }
 
+// 2
+
+$a = rand(0, 15);
+
+switch ($a) {
+    case 0:
+        echo $a;
+        break;
+    case 1:
+        echo $a;
+        break;
+    case 2:
+        echo $a;
+        break;
+    case 3:
+        echo $a;
+        break;
+    case 4:
+        echo $a;
+        break;
+    case 5:
+        echo $a;
+        break;
+    case 6:
+        echo $a;
+        break;
+    case 7:
+        echo $a;
+        break;
+    case 8:
+        echo $a;
+        break;
+    case 9:
+        echo $a;
+        break;
+    case 10:
+        echo $a;
+        break;
+    case 11:
+        echo $a;
+        break;
+    case 12:
+        echo $a;
+        break;
+    case 13:
+        echo $a;
+        break;
+    case 14:
+        echo $a;
+        break;
+    case 15:
+        echo $a;
+        break;
+}
+
